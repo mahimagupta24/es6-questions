e8c00b55308f8b130ceb306b482bd48d9fb1da9e
mahimagupta24 <mahimagupta2401@gmail.com>
2023-02-28 17:09:04 +0530

    refactor code

--- p4/average.js
+++ p4/average.js
@@ -1,11 +1,11 @@
 //Write an ES6 function that takes an array of numbers and returns the average of all the numbers. Avoid using in-built methods.
 
 const calculateAverage = (arr) => {
-    let average = 0;
+    let sum = 0;
     for(let i =0;i <arr.length;i++){
-        average = Math.trunc(average+i)/2
+    sum += arr[i]
     }
-    return average
+    return sum/arr.length
 }
 
 console.log(calculateAverage([1, 2, 3, 4, 5]));
