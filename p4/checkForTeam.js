c9d59fbe0c4a36f9b63939099599a26969b67164
mahimagupta24 <mahimagupta2401@gmail.com>
2023-02-28 17:02:23 +0530

    add one more solution to check for teams question

--- p4/checkForTeam.js
+++ p4/checkForTeam.js
@@ -20,10 +20,10 @@
 const checkForTeam =(student)=>({...student,team:student.team??'A'})
 
 console.log(checkForTeam({ firstName: "Penn", lastName: "Ma" }));
-// {firstName: 'Penn', lastName: 'Ma', team: A}
+
 
 console.log(checkForTeam({ firstName: "John", lastName: "Dee", team: "B" }));
-// {firstName: 'John', lastName: 'Dee', team: B}
+
 
 console.log(checkForTeam({ firstName: "Priya", lastName: "Raj" }));
-// {firstName: 'Priya', lastName: 'Raj', team: A}
+
